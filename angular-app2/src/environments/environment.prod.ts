bffe20575992dc9f5a12cd250b292ec0727a4bef
pateriyadeepali <pateriyadeepali1008@gmail.com>
2025-10-14 12:44:23 +0530

    Reorganize Angular project into top-level folder

--- angular-app2/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrlDotnet: 'http://192.168.0.178:5258',
-  apiUrlNode: 'http://192.168.0.178:4000'
-};
--- angular-app2/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrlNode: 'http://localhost:4000',
-  apiUrlDotnet: 'http://localhost:5258'
-};
